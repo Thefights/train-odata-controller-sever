e8116a8d78b950785627c93e203644b32d9bc676
QuachKhang <phuckhang1088@gmail.com>
2025-03-22 15:24:20 +0700

    add CRUD for ProductManagementController

---
+++ Controllers/ProductManagementController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using train_odata_controller_sever.Data;
+using train_odata_controller_sever.Models;
+
+namespace train_odata_controller_sever.Controllers
+{
+    [Route("product-management")]
+    [ApiController]
+    public class ProductManagementController (ApplicationDbContext _dbContext) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await _dbContext.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct (Product product)
+        {
+            await _dbContext.Products.AddAsync(product);
+            await _dbContext.SaveChangesAsync();
+            return Ok(product);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct( Product product)
+        {
+             _dbContext.Products.Update(product);
+            await _dbContext.SaveChangesAsync();
+            return Ok(product);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _dbContext.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Products.Remove(product);
+            await _dbContext.SaveChangesAsync();
+            return Ok(product);
+        }
+
+
+    }
+}
