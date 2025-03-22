4462fa0316662c674a932c28b3d0d6b128c8ec96
QuachKhang <phuckhang1088@gmail.com>
2025-03-22 14:49:53 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace train_odata_controller_sever.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
