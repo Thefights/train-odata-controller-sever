dc0f8a484769c1ce357406be3454cac7efa079cd
QuachKhang <phuckhang1088@gmail.com>
2025-03-22 14:59:14 +0700

    first config source

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace train_odata_controller_sever.Data
+{
+    public class ApplicationDbContext (DbContextOptions options) :DbContext(options)
+    {
+        //public DbSet<Train> Trains { get; set; } = null!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using train_odata_controller_sever.Data;
+
 namespace train_odata_controller_sever
 {
     public class Program
@@ -8,6 +12,28 @@ namespace train_odata_controller_sever
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Train Signal API",
+                    Version = "v1",
+                    Description = "API for Train Signal IR Server",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Quach Khang",
+                        Email = "phuckhang1088@gmail.com",
+                        Url = new Uri("https://yourwebsite.com")
+                    }
+                });
+            });
+
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
 
             var app = builder.Build();
 
@@ -20,6 +46,13 @@ namespace train_odata_controller_sever
             }
 
             app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Train OData Controller API v1");
+                c.RoutePrefix = "swagger"; // Swagger UI available at: https://localhost:5001/swagger
+            });
+
             app.UseRouting();
 
             app.UseAuthorization();
